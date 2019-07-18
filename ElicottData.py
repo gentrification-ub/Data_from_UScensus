eb8192f01b8463d8c27f5c123b6428f850346e42
jv11699 <janvolta@buffalo.edu>
2019-07-18 18:03:51 -0400

    - update

--- ElicottData.py
+++ ElicottData.py
@@ -12,26 +12,116 @@ Once reproduced, all the data will be turned into csv files for visualization
 import csv
 import json
 import requests
+from itertools import zip_longest
 
 # =============================================================================
-api_key = '3fad1f7c603dfb341edd045495a58a7c0e77f15c' # API key
+api_key = '3fad1f7c603dfb341edd045495a58a7c0e77f15c'  # API key
 # The parameters are set to have my API key and the geography level down to the block level of Ellicott neighborhood
-parameters = {"key": api_key, "for":"block group:1,2,4", "in": "state:36+county:029+tract:001402"}
-api_base_url = lambda acs_year: 'https://api.census.gov/data/' + acs_year + '/acs/acs5?' # API call link
+parameters = {"key": api_key, "for": "block group:1,2,4", "in": "state:36+county:029+tract:001402"}
+num_blocks = 3
+api_base_url = lambda acs_year: 'https://api.census.gov/data/' + acs_year + '/acs/acs5?'  # API call link
 # ==============================================================================
 
+def update_response_parameters(dict_param ,base_url): # returns response
+    parameters.update(dict_param )
+    response = requests.get(url=base_url, params=parameters)
+    return response.json()
+
+def main():
+
+    # these are the variables needed for the gentrification early warning system
+    variables = {"Total population": "B01003_001E",
+                 # race
+                 "Total number of person(only Black or African American)": "B02001_003E",
+                 "Total number of person(only White)": "B02001_002E",
+                 "Total number of person(only Hisapnic)": "B03002_012E",
+                 # household type
+                 "Total Number of Housing Units": "B25001_001E",
+                 "Total Number of Renter per housing unit": "B25003_003E",
+                 "Total Number of Vacant Housing Units": "B25002_003E",
+                 # poverty
+                 "Total Population for whom poverty status is determined" : "C17002_001E",
+                 "Total below povery line (population whose poverty level is determined)": ["C17002_002E", "C17002_003E"],
+                 # gross rent as income
+                 "Total Median Gross Rent As A Percentage Of Household Income In The Past 12 Months (Dollars)":
+                                                                                                        "B25071_001E",
+                 # educational attainment
+                 "Total Population that is 25 years and older ": "B15003_001E",
+                 "Total Population 25 years and older that have less than a college education":
+                        ["B15003_0" + str(i).zfill(2) + "E" for i in range(2, 19)]}
+
+
+
+
+    '''
+    -This calculates the percentages on a block level , then puts them into a csv file
+    file header when writing onto a csv: 
+        | Geography (block group #)| Percent of the variable compared to the total | Variables.. 
+    '''
+    
+    with open('UBgentrification_data.csv', mode='w') as data:
+        data_writer = csv.writer(data, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+        for var_title, var_code in variables.items():
+            data_list = [] # ini
+            year = 2016
+            # calculates whether there are multiple variables that needed to be added to get the whole percentage
+            if isinstance(var_code, list): #checks if the pair is a list
+                data_list = [0] * num_blocks # initializes the array to zero so that it can accumulate totals
+                for codes in var_code:
+                    parameters.update( {"get": codes } )
+                    response = requests.get( url= api_base_url(str(year)), params=parameters)
+                    json = response.json()
+                    for block_count in range(1, len(json)): # it starts at 1 because that's after the header
+                        data_list[block_count-1] += int(json[block_count][0])
+            else:
+                parameters.update( {"get": var_code})
+                response = requests.get(api_base_url(str(year)), params=parameters)
+                json = response.json()
+       '''  for year in range(2013,2018):
+            for var_title, var_code in variables.items():
+                data_list = []  # initializing empty list
+                if isinstance(var_code, list):
+                    data_list = [0] * 3
+                    for codes in var_code:
+                        parameters.update({"get": codes})
+                        response = requests.get(api_base_url(str(year)), params=parameters)
+                        json = response.json()
+                        for block_count in range(1, len(json)):
+                            data_list[block_count-1] += int(json[block_count][0])
+    
+                else:
+                    parameters.update({"get": var_code})
+                    response = requests.get(api_base_url(str(year)), params=parameters)
+                    json = response.json()
+                print(data_list)
+                for block in range(1,len(json)):
+                    var_data = json[block][0] if len(data_list) == 0 else data_list[block-1]
+                    data_tract = json[block][3]
+                    data_blockGrp = json[block][4]
+                    data_writer.writerow(["blank", var_title, var_data, data_tract, data_blockGrp, year]) '''
+
+    parameters.update( {"get": "B02001_003E"} ) # till 18
+
+
+
+    response = requests.get(api_base_url('2016'), params = parameters)
+    json = response.content.decode("utf-8")
+    print(json)
+
+if __name__ == '__main__' :
+    main()
 '''
 THIS IS TO CALCULATE ON A NEIGHBORHOOD LEVEL 
 
 def ACScalculate_percent(variable_sample, variable_total,year, num_block = 3):
-    
+
     assumes that 
     :param variable_sample: 
     :param variable_total: 
     :param year 
     :param num_block
     :return:
-    
+
     parameters.update( {"get": variable_sample + "," + variable_total})
     response = requests.get(api_base_url(str(year)), params=parameters)
     json = response.json()
@@ -46,67 +136,8 @@ def ACScalculate_percent(variable_sample, variable_total,year, num_block = 3):
             json = response.json()
             for i in range(1,num_block):
                 var_count =+ json[i][0]
-       
+
     else:
 
     return (var_count / total_var_count) * 100
 '''
-
-# these are the variables needed for the gentrification early warning system
-
-variables = {"Total population": "B01003_001E",
-             # race
-             "Total number of person(only Black or African American)": "B02001_003E",
-             "Total number of person(only White)": "B02001_002E",
-             "Total number of person(only Hisapnic)": "B03002_012E",
-             # household type
-             "Total Number of Housing Units": "B25001_001E",
-             "Total Number of Renter per housing unit": "B25003_003E",
-             "Total Number of Vacant Housing Units": "B25002_003E",
-             # poverty
-             "Total Population for whom poverty status is determined" : "C17002_001E",
-             "Total below povery line (population whose poverty level is determined)": ["C17002_002E", "C17002_003E"],
-             # gross rent as income
-             "Total Median Gross Rent As A Percentage Of Household Income In The Past 12 Months (Dollars)":
-                                                                                                    "B25071_001E",
-             # educational attainment
-             "Total Population that is 25 years and older ": "B15003_001E",
-             "Total Population 25 years and older that have less than a college education":
-                    ["B15003_0" + str(i).zfill(2) + "E" for i in range(2, 19)]}
-
-
-# This calculates the percentages on a block level , then puts them into a csv file
-
-with open('UBgentrification_data.csv', mode='w') as data:
-    data_writer = csv.writer(data, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
-    data_writer.writerow(["Percent compared to the total", "Variable Title", "data", "tract", "block group", "year"])
-    for year in range(2013,2018):
-        for var_title, var_code in variables.items():
-            data_list = []  # initializing empty list
-            if isinstance(var_code, list):
-                data_list = [0] * 3
-                for codes in var_code:
-                    parameters.update({"get": codes})
-                    response = requests.get(api_base_url(str(year)), params=parameters)
-                    json = response.json()
-                    for block_count in range(1, len(json)):
-                        data_list[block_count-1] += int(json[block_count][0])
-
-            else:
-                parameters.update({"get": var_code})
-                response = requests.get(api_base_url(str(year)), params=parameters)
-                json = response.json()
-            print(data_list)
-            for block in range(1,len(json)):
-                var_data = json[block][0] if len(data_list) == 0 else data_list[block-1]
-                data_tract = json[block][3]
-                data_blockGrp = json[block][4]
-                data_writer.writerow(["blank", var_title, var_data, data_tract, data_blockGrp, year])
-
-parameters.update( {"get": "B02001_003E"} ) # till 18
-
-
-
-response = requests.get(api_base_url('2016'), params = parameters)
-json = response.content.decode("utf-8")
-print(json)
