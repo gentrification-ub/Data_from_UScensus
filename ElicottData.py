e1f7d2877064be803487d2b25bf37bf27f550922
jv11699 <janvolta@buffalo.edu>
2019-07-23 00:56:05 -0400

    - percentages are added in - unit testing still required to check whether it has the right data

--- ElicottData.py
+++ ElicottData.py
@@ -13,7 +13,6 @@ import csv
 import json
 import requests
 from itertools import zip_longest
-from functools import partial
 # =============================================================================
 api_key = '3fad1f7c603dfb341edd045495a58a7c0e77f15c'  # API key
 # The parameters are set to have my API key and the geography level down to the block level of Ellicott neighborhood
@@ -21,7 +20,13 @@ parameters = {"key": api_key, "for": "block group:1,2,4", "in": "state:36+county
 num_blocks = 3
 api_base_url = lambda acs_year: 'https://api.census.gov/data/' + acs_year + '/acs/acs5?'  # API call link
 # ==============================================================================
-
+def get_total_var_data(var_code,year):
+    if isinstance(var_code, list):
+        new_var_code = var_code[0][:-3]
+    else:
+        new_var_code = var_code[:-3]
+    new_var_code = new_var_code + "01E"
+    return update_response_parameters(dict_param={"get": new_var_code}, base_url=api_base_url(str(year)))
 def update_response_parameters(dict_param ,base_url): # returns response
     parameters.update(dict_param)
     response = requests.get(url=base_url, params=parameters)
@@ -38,8 +43,10 @@ def main():
                  # household type
                  "Total Number of Housing Units": "B25001_001E",
                  "Total Number of Renter per housing unit": "B25003_003E",
+                 "Total Number of Owner per housing unit": "B25003_002E",
                  "Total Number of Vacant Housing Units": "B25002_003E",
-                 # poverty
+
+                 # poverty ...... to get total u subtract the last part yes?
                  "Total Population for whom poverty status is determined" : "C17002_001E",
                  "Total below povery line (population whose poverty level is determined)": ["C17002_002E", "C17002_003E"],
                  # gross rent as income
@@ -48,10 +55,12 @@ def main():
                  # educational attainment
                  "Total Population that is 25 years and older ": "B15003_001E",
                  "Total Population 25 years and older that have less than a college education":
-                        ["B15003_0" + str(i).zfill(2) + "E" for i in range(2, 19)]}
+                        ["B15003_0" + str(i).zfill(2) + "E" for i in range(2, 19)],
 
+                 # rent/housing price
 
-
+                 "Total Median contract rent (DOLLARS)": "B25058_001E",
+                 "Total Median Housing values (DOLLARS)": "B25077_001E"}   # / Owner occupied housing units
 
     # This calculates the percentages on a block level , then puts them into a csv file
     with open('UBgentrification_data.csv', mode='w') as data:
@@ -74,13 +83,17 @@ def main():
                 json_data = update_response_parameters(dict_param={"get": var_code}, base_url=api_base_url(str(year)))
 
             var_data = [var_title]  # the is inside the for loop since it needs to keep adding variables onto the header
-
+            perc_data = ["Percent " + var_title]
             for block in range(1, len(json_data)):
                 var_data.append(json_data[block][0] if len(data_list) == 0 else data_list[block-1])
+                perc_data.append(
+                    ((float(var_data[len(var_data)-1]) / float(get_total_var_data(var_code, year)[block][0])) * 100)
+                )
                 tract_list.append(json_data[block][3]) if len(tract_list) <= num_blocks else None
                 block_list.append(json_data[block][4]) if len(block_list) <= num_blocks else None # !! THESE ARE ONLY ADDED CUZ THERES NO YEARS
 
             var_list.append(var_data)
+            var_list.append(perc_data)
 
         result = zip_longest(tract_list, block_list, *var_list, fillvalue='None')
         data_writer.writerows(result)
