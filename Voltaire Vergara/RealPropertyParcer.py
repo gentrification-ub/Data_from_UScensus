d24c27b89a0d7a9eb0db4b5d26323ec51886b58c
jv11699 <janvolta@buffalo.edu>
2019-11-08 21:54:04 -0500

    added RealPropertyParcer.py

--- Voltaire Vergara/RealPropertyParcer.py
+++ Voltaire Vergara/RealPropertyParcer.py
@@ -1,20 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Created By  : Voltaire Vergara
+# Created Date: 11/8/2019
+# =============================================================================
+"""
+The aim of this file is to reproduce a beautiful soup object that contains
+the tax history data of an individual through the erie county tax parcel website. Here we use an individiuals
+unique SBL to query information
+"""
+
 import requests
 from bs4 import BeautifulSoup
-body = {'txtowner':'wyse',  # WE CAN CHANGE THIS TO SBL BUT THIS IS OWNER NAME
-        'showHistory': 'y'} # <-- use 'query' not `Search'
-url2 = 'https://paytax.erie.gov/webprop/' #url we needed adding to
-url = 'https://paytax.erie.gov/webprop/property_info_results.asp' # search website
-con = requests.post(url, data=body)
-soup = BeautifulSoup(con.content, 'lxml')
-details_link = []
-for a in soup.find_all('a', href=True):
-    details_link.append(a['href'])
-    #print ("Found the URL:", a['href'])  # what we found here we need to add to url + "FOUND URL'
 
-con2 = requests.post(url2+details_link[0])
-soup2 = BeautifulSoup(con2.content, 'lxml')
-for a in soup2.find_all('a', href=True):
-    #details_link.append(a['href'])
-    print ("Found the URL:", a['href'])  # what we found here we need to add to url + "FOUND URL'
+def grab_source_SBL(sbl):
+    """
+    Queries the Real Property Parcer of erie county(Specifically only SBL values), which can be found on this link:
+                            http://www2.erie.gov/ecrpts/index.php?q=real-property-parcel-search
+    This function will return a beautiful soup object of the source code of a SBL's tax History.
+    :param sbl: SBL value that we are searching for
+    :return: a Beautiful soup object of the tax extry history of that SBL
+    """
+    body = {'txtsbl': sbl,  # WE CAN CHANGE THIS TO SBL BUT THIS IS OWNER NAME
+            'showHistory': 'y'}
+    url2 = 'https://paytax.erie.gov/webprop/'  # url we needed adding to since this is the root link
+    url = 'https://paytax.erie.gov/webprop/property_info_results.asp'  # search website
+    link = requests.post(url, data=body)
+    source_soup = BeautifulSoup(link.content, 'lxml')
+    details_link = []
+    for a in source_soup.find_all('a', href=True):
+        details_link.append(a['href'])
 
-#print(con2.url) # Taxpayment history starts iwth ecgov_pgl.asp <- use this by url2 + the found url
+    # if there's only one element then that means that an SBL does not exist
+    if len(details_link) > 1:
+        # we can assure that SBL's are unique and always at the head of the list
+        con2 = requests.post(url2+details_link[0])
+        details_soup = BeautifulSoup(con2.content, 'lxml')
+        for a in details_soup.find_all('a', href=True):
+            details_link.append(a['href'])
+        TAXLINK = url2 + details_link[3]
+        return BeautifulSoup(requests.post(TAXLINK).content, 'lxml')
+
+    else:
+        raise Exception("SBL does not exist")
+
+
+
+print(grab_source_SBL('100.23-2-12./S'))
